5daae4c99f610e81abc97b6e66d8ac9feb779bc8
pj <pj.vandenbussche@gmail.com>
2024-12-11 18:36:07 +0100

    fix: start fold w/ empty freq

--- src/Day11.hs
+++ src/Day11.hs
@@ -10,12 +10,11 @@ parse :: String -> Freq
 parse = foldl (\m s -> let n = read s in M.insert n 1 m) M.empty . words
 
 update :: Freq -> Int -> Int -> Freq
-update freq stone count = incStones (decStone stone freq) stones
+update freq stone count = incStones freq stones
   where
     stones = case stone of
       0 -> [1]
       s -> let n = countDigits s in if even n then splitStone s n else [s * 2024]
-    decStone = M.adjust (\n -> n - count)
     incStones = foldr (M.alter (\case Just n -> Just (n + count); Nothing -> Just count))
     countDigits n = if n == 0 then 0 else 1 + countDigits (div n 10)
     splitStone stone n = let q = 10 ^ (n `div` 2) in [stone `div` q, stone `mod` q]
@@ -23,7 +22,7 @@ update freq stone count = incStones (decStone stone freq) stones
 blinks :: Int -> String -> String
 blinks n input = show $ sum $ M.filter (> 0) $ iterate blink (parse input) !! n
   where
-    blink freq = M.foldlWithKey' (\c k v -> if v > 0 then update c k v else c) freq freq
+    blink = M.foldlWithKey' (\c k v -> if v > 0 then update c k v else c) M.empty
 
 part1 :: String -> String
 part1 = blinks 25
