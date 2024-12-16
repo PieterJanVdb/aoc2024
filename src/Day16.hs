95d031b825684580bd7b6c6b0bd83e20f05ee10c
pj <pj.vandenbussche@gmail.com>
2024-12-17 00:58:05 +0100

    feat: day16 part2

--- src/Day16.hs
+++ src/Day16.hs
@@ -3,120 +3,55 @@ module Day16 (part1, part2) where
 import Data.List (elemIndex, foldl', sortBy)
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as M
-import Data.Maybe (isJust, isNothing, listToMaybe, mapMaybe)
+import Data.Maybe (catMaybes, isJust, isNothing, listToMaybe, mapMaybe)
 import Data.Set qualified as S
 import Debug.Trace (trace)
-import Utils.Grid (Coord, Grid, makeGrid)
+import Utils.Grid (Coord, Direction (..), Grid, makeGrid, neighbour)
 
-data Direction = N | E | S | W deriving (Enum, Eq, Show)
+type Path = [Coord]
 
-data Entry = Entry Char (Maybe Int) Direction deriving (Eq, Show)
+type Queue = [(Path, Direction, Int)]
 
-data Neighbour = Neighbour Coord Entry Direction deriving (Show)
+type Seen = Map (Coord, Direction) Int
 
-ex :: String
-ex =
-  "###############\n\
-  \#.......#....E#\n\
-  \#.#.###.#.###.#\n\
-  \#.....#.#...#.#\n\
-  \#.###.#####.#.#\n\
-  \#.#.#.......#.#\n\
-  \#.#.#####.###.#\n\
-  \#...........#.#\n\
-  \###.#.#####.#.#\n\
-  \#...#.....#.#.#\n\
-  \#.#.#.###.#.#.#\n\
-  \#.....#...#.#.#\n\
-  \#.###.#.#.#.#.#\n\
-  \#S..#.....#...#\n\
-  \###############"
-
-ex1 :: String
-ex1 =
-  "#################\n\
-  \#...#...#...#..E#\n\
-  \#.#.#.#.#.#.#.#.#\n\
-  \#.#.#.#...#...#.#\n\
-  \#.#.#.#.###.#.#.#\n\
-  \#...#.#.#.....#.#\n\
-  \#.#.#.#.#.#####.#\n\
-  \#.#...#.#.#.....#\n\
-  \#.#.#####.#.###.#\n\
-  \#.#.#.......#...#\n\
-  \#.#.###.#####.###\n\
-  \#.#.#...#.....#.#\n\
-  \#.#.#.#####.###.#\n\
-  \#.#.#.........#.#\n\
-  \#.#.#.#########.#\n\
-  \#S#.............#\n\
-  \#################"
-
-parse :: [String] -> Grid Char
-parse = makeGrid
-
-prepare :: Grid Char -> Grid [Entry]
-prepare grid = M.map (\c -> [Entry c (if c == 'S' then Just 0 else Nothing) E]) $ M.filter (/= '#') grid
-
-getNeighbours :: Grid [Entry] -> (Coord, Entry) -> [Neighbour]
-getNeighbours grid (_, Entry _ Nothing _) = []
-getNeighbours grid ((r, c), Entry _ (Just cs) dir) =
-  concat $
-    mapMaybe
-      (\d -> let c = neighbour d in case M.lookup c grid of Just e -> Just $ map (\x -> Neighbour c x d) e; _ -> Nothing)
-      (filter (/= inverse dir) (enumFrom N))
+findSeatsAndScore :: Grid Char -> Maybe (Int, Int)
+findSeatsAndScore grid = go grid initQueue M.empty [] maxBound
   where
-    neighbour = \case N -> (r - 1, c); E -> (r, c + 1); S -> (r + 1, c); W -> (r, c - 1)
-    inverse dir = case dir of N -> S; E -> W; S -> N; W -> E
+    (start, _) = M.findMax $ M.filter (== 'S') grid
+    initQueue = [([start], E, 0)]
 
-getNextUnvisited :: Grid [Entry] -> Maybe (Coord, Entry)
-getNextUnvisited grid =
-  listToMaybe $
-    dropWhile (\(_, Entry _ s _) -> isNothing s) $
-      sortBy (\(_, Entry _ s _) (_, Entry _ s' _) -> compare s s') $
-        M.foldlWithKey' (\acc k vs -> acc ++ map (k,) vs) [] grid
+    pop :: Queue -> Maybe ((Path, Direction, Int), [(Path, Direction, Int)])
+    pop queue = case sortBy (\(_, _, a) (_, _, b) -> compare a b) queue of
+      [] -> Nothing
+      (x : xs) -> Just (x, xs)
 
-getScore :: Direction -> Direction -> Int
-getScore from to = if from == to then 1 else 1001
-
--- MAINTAIN ALL POSSIBLE DIRECTIONS WHERE SCORE = SCORE' (TUPLE OF (Int, Direction))
-updateNeighbour :: (Grid [Entry], Map Coord [Coord]) -> (Coord, Int, Direction) -> Neighbour -> (Grid [Entry], Map Coord [Coord])
-updateNeighbour (unvisited, visited) (source, current, from) n = case n of
-  (Neighbour coord (Entry char Nothing dir) to) ->
-    ( M.adjust (const [Entry char (Just (current + getScore from to)) to]) coord unvisited,
-      M.alter (Just . maybe [source] (source :)) coord visited
-    )
-  (Neighbour coord e@(Entry char (Just score) dir) to) ->
-    let score' = current + getScore from to
-        entry' = Entry char (Just (min score' score)) (if score' < score then to else dir)
-        newEntry = if (score' - 1000) == score || score' == (score - 1000) then Just $ Entry char (Just score') to else Nothing
-     in ( M.adjust (\xs -> let xs' = (entry' : filter (== e) xs) in maybe xs' (: xs') newEntry) coord unvisited,
-          if score' <= score then M.alter (Just . maybe [source] (source :)) coord visited else visited
-        )
-
-findShortestPaths :: (Grid [Entry], Map Coord [Coord]) -> Maybe (Int, Map Coord [Coord])
-findShortestPaths (unvisited, visited) = case getNextUnvisited unvisited of
-  Just (_, Entry 'E' (Just s) _) -> Just (s, visited)
-  Just (c, e@(Entry _ (Just current) from)) -> findShortestPaths (M.adjust (filter (/= e)) c unvisited', visited')
-    where
-      neighbours = getNeighbours unvisited (c, e)
-      (unvisited', visited') = foldl' (\(u, v) n -> updateNeighbour (u, v) (c, current, from) n) (unvisited, visited) neighbours
-  _ -> Nothing
-
-extractPlacesCount :: Map Coord [Coord] -> Coord -> Int
-extractPlacesCount mapping from = length $ S.fromList $ go from [] (M.lookup from mapping)
-  where
-    go x visited Nothing = x : visited
-    go x visited (Just ns) = concatMap (\n -> go n (x : visited) (M.lookup n mapping)) ns
+    go :: Grid Char -> Queue -> Seen -> [Path] -> Int -> Maybe (Int, Int)
+    go grid queue seen paths minScore = case pop queue of
+      Nothing -> Nothing
+      Just ((path@(p : ps), dir, score), queue') ->
+        case grid M.! p of
+          'E' | score > minScore -> Just (minScore, length $ S.fromList $ concat paths)
+          'E' -> go grid queue' seen (path : paths) score
+          _ | stop -> go grid queue' seen paths minScore
+          _ -> go grid (toProcess ++ queue') seen' paths minScore
+        where
+          stop = maybe False (< score) (M.lookup (p, dir) seen)
+          seen' = M.alter (Just . const score) (p, dir) seen
+          toProcess =
+            catMaybes
+              [ (neighbour p dir : path, dir, score + 1) <$ M.lookup (neighbour p dir) grid,
+                Just (path, turnLeft dir, score + 1000),
+                Just (path, turnRight dir, score + 1000)
+              ]
+          turnLeft = \case N -> W; E -> N; S -> E; W -> S
+          turnRight = \case N -> E; E -> S; S -> W; W -> N
 
 part1 :: [String] -> String
-part1 input = maybe "No paths could be found" (show . fst) (findShortestPaths (prepare $ parse input, M.empty))
-
-solve2 :: [String] -> Grid [Coord]
-solve2 input = maybe M.empty snd (findShortestPaths (prepare $ parse input, M.empty))
+part1 input = maybe "No paths could be found" (show . fst) (findSeatsAndScore grid)
+  where
+    grid = M.filter (/= '#') $ makeGrid input
 
 part2 :: [String] -> String
-part2 input = maybe "No paths could be found" (show . (`extractPlacesCount` end) . snd) (findShortestPaths (prepare grid, M.empty))
+part2 input = maybe "No paths could be found" (show . snd) (findSeatsAndScore grid)
   where
-    grid = parse input
-    (end, _) = M.findMax $ M.filter (== 'E') grid
+    grid = M.filter (/= '#') $ makeGrid input
--- src/Utils/Grid.hs
+++ src/Utils/Grid.hs
@@ -1,4 +1,14 @@
-module Utils.Grid (Grid, Coord, makeGrid, makeGridWith, invertGrid, invertGridWithFilter) where
+module Utils.Grid
+  ( Grid,
+    Coord,
+    Direction (..),
+    makeGrid,
+    makeGridWith,
+    invertGrid,
+    invertGridWithFilter,
+    neighbour,
+  )
+where
 
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as Map
@@ -6,6 +16,8 @@ import Data.Map.Strict qualified as Map
 type Coord = (Int, Int)
 
 type Grid a = Map Coord a
+
+data Direction = N | E | S | W deriving (Enum, Eq, Ord, Show)
 
 makeGridWith :: (a -> b) -> [[a]] -> Grid b
 makeGridWith f xs = Map.fromList $ concatMap (uncurry makeRow) (zip [0 ..] xs)
@@ -20,3 +32,6 @@ invertGridWithFilter filter g = Map.fromListWith (++) [(v, [k]) | (k, v) <- Map.
 
 invertGrid :: (Ord a) => Grid a -> Map a [Coord]
 invertGrid = invertGridWithFilter (const True)
+
+neighbour :: Coord -> Direction -> Coord
+neighbour (r, c) = \case N -> (r - 1, c); E -> (r, c + 1); S -> (r + 1, c); W -> (r, c - 1)
