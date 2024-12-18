9e3289e02631a3ea929f2dd5acd27afc4ce9a9a7
pj <pj.vandenbussche@gmail.com>
2024-12-18 01:57:19 +0100

    feat: day17 part2

--- src/Day17.hs
+++ src/Day17.hs
@@ -2,7 +2,9 @@ module Day17 (part1, part2) where
 
 import Data.Bifunctor (bimap)
 import Data.Bits (xor)
+import Data.Char (digitToInt)
 import Data.List (foldl', intercalate)
+import Data.Maybe (listToMaybe, mapMaybe)
 import Data.Vector (Vector)
 import Data.Vector qualified as V
 import Debug.Trace (trace)
@@ -15,44 +17,37 @@ import Utils.Lists (chunks)
 
 lexer = makeTokenParser haskellDef
 
-ex :: String
-ex =
-  "Register A: 117440\n\
-  \Register B: 0\n\
-  \Register C: 0\n\
-  \\n\
-  \Program: 0,3,5,4,3,0"
-
 data Opcode = Adv | Bxl | Bst | Jnz | Bxc | Out | Bdv | Cdv deriving (Show)
 
-type Operand = Int
+type Operand = Integer
 
 type Instruction = (Opcode, Operand)
 
-type Output = [Int]
+type Output = [Integer]
 
 data Computer = Computer
-  { ra :: Int,
-    rb :: Int,
-    rc :: Int,
+  { ra :: Integer,
+    rb :: Integer,
+    rc :: Integer,
     pointer :: Int,
     program :: Vector Instruction,
-    output :: [Int]
+    rawProgram :: [Integer],
+    output :: [Integer]
   }
   deriving (Show)
 
 parser :: Parser Computer
 parser =
-  (\ra rb rc program -> Computer (fromInteger ra) (fromInteger rb) (fromInteger rc) 0 (parseProgram program) [])
+  (\ra rb rc program -> Computer ra rb rc 0 (parseProgram program) program [])
     <$> (string "Register A: " *> integer lexer)
     <*> (string "Register B: " *> integer lexer)
     <*> (string "Register C: " *> integer lexer)
     <*> (string "Program: " *> (integer lexer `sepBy1` symbol lexer ","))
   where
     parseOpcode = \case 0 -> Adv; 1 -> Bxl; 2 -> Bst; 3 -> Jnz; 4 -> Bxc; 5 -> Out; 6 -> Bdv; 7 -> Cdv
-    parseProgram xs = V.fromList $ map (\[x, y] -> (parseOpcode x, fromInteger y)) (chunks 2 xs)
+    parseProgram xs = V.fromList $ map (\[x, y] -> (parseOpcode x, y)) (chunks 2 xs)
 
-combo :: Computer -> Int -> Int
+combo :: Computer -> Integer -> Integer
 combo c@(Computer {ra, rb, rc}) operand = case operand of
   l | l <= 3 -> l
   4 -> ra
@@ -78,7 +73,7 @@ bst :: Computer -> Operand -> Computer
 bst c operand = move $ c {rb = combo c operand `mod` 8}
 
 jnz :: Computer -> Operand -> Computer
-jnz c@(Computer {ra}) operand = if ra == 0 then move c else c {pointer = operand}
+jnz c@(Computer {ra}) operand = if ra == 0 then move c else c {pointer = fromInteger operand}
 
 bxc :: Computer -> Computer
 bxc c@(Computer {rb, rc}) = move $ c {rb = rb `xor` rc}
@@ -89,17 +84,25 @@ out c@(Computer {output}) operand = move $ c {output = (combo c operand `mod` 8)
 run :: Computer -> Output
 run c@(Computer {pointer, program, output}) = case program V.!? pointer of
   Nothing -> reverse output
-  Just (Adv, operand) -> let n = adv c operand in trace ("ADV -> " <> show n) run n
-  Just (Bdv, operand) -> let n = bdv c operand in trace ("BDV -> " <> show n) run n
-  Just (Cdv, operand) -> let n = cdv c operand in trace ("CDV -> " <> show n) run n
-  Just (Bxl, operand) -> let n = bxl c operand in trace ("BXL -> " <> show n) run n
-  Just (Bst, operand) -> let n = bst c operand in trace ("BST -> " <> show n) run n
-  Just (Jnz, operand) -> let n = jnz c operand in trace ("JNZ -> " <> show n) run n
-  Just (Out, operand) -> let n = out c operand in trace ("OUT -> " <> show n) run n
-  Just (Bxc, _) -> let n = run (bxc c) in trace ("BXC -> " <> show n) n
+  Just (Adv, operand) -> run $ adv c operand
+  Just (Bdv, operand) -> run $ bdv c operand
+  Just (Cdv, operand) -> run $ cdv c operand
+  Just (Bxl, operand) -> run $ bxl c operand
+  Just (Bst, operand) -> run $ bst c operand
+  Just (Jnz, operand) -> run $ jnz c operand
+  Just (Out, operand) -> run $ out c operand
+  Just (Bxc, _) -> run $ bxc c
 
 printOutput :: Output -> String
 printOutput out = intercalate "," (map show out)
+
+lowestA :: Computer -> Int -> Integer -> Maybe Integer
+lowestA c@(Computer {rawProgram}) n a0 | n > 16 = Just a0
+lowestA c@(Computer {rawProgram}) n a0 = listToMaybe $ mapMaybe (lowestA c (n + 1)) viable
+  where
+    a1 = a0 * 8
+    target = drop (length rawProgram - n) rawProgram
+    viable = filter (\a -> run c {ra = a} == target) [a1 .. a1 + 7]
 
 part1 :: String -> String
 part1 input = case parse parser "computer" input of
@@ -107,4 +110,6 @@ part1 input = case parse parser "computer" input of
   Right computer -> printOutput $ run computer
 
 part2 :: String -> String
-part2 input = ""
+part2 input = case parse parser "computer" input of
+  Left e -> error (show e)
+  Right computer -> maybe "Oops" show (lowestA computer 1 0)
